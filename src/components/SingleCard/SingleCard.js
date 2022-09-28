8fddc34a76dab4bf685eff2d9471d23c717449eb
jubayer44 <jubayerahmedshamim44@gmail.com>
2022-09-29 00:28:48 +0600

    card-design-completed

--- src/components/SingleCard/SingleCard.js
+++ src/components/SingleCard/SingleCard.js
@@ -5,11 +5,13 @@ const SingleCard = ({ sport }) => {
     const { name, img, description, followers, time } = sport;
     return (
         <div className='single-card'>
+            <div className='info'>
             <img src={img} alt="" />
             <h3>{name}</h3>
             <p>{description}</p>
             <p><strong>Followers : {followers}</strong></p>
             <p><strong>Time required : {time}</strong></p>
+            </div>
             <button>Add to list</button>
         </div>
     );
