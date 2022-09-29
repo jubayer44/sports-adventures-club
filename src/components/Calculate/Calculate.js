f972663f18f191538e0a34014038f8fc618fd3fb
jubayer44 <jubayerahmedshamim44@gmail.com>
2022-09-29 08:30:56 +0600

    break-time-section-calculate-completed

--- src/components/Calculate/Calculate.js
+++ src/components/Calculate/Calculate.js
@@ -1,26 +1,29 @@
+import { useState } from 'react';
 import './Calculate.css'
 
 const Calculate = ({sportsData}) => {
+    const [breakTime, setBreakTime] = useState(0)
+    
     let time = 0;
     for(const data of sportsData){
         
         time = time + data.time;
     }
-
     const handleBreak = (e) => {
-        console.log(e.target.value);
+        setBreakTime((breakTime) => breakTime + parseFloat(e.target.innerText))
+        
     }
-
+    console.log(breakTime);
     return (
         <div>
             <div style={{marginTop: '40px'}}>
             <h3>Add A Break</h3>
             <div className='break-min'>
-                <p onClick={handleBreak}>10m</p>
-                <p>20m</p>
-                <p>30m</p>
-                <p>40m</p>
-                <p>50m</p>
+            <p><span onClick={handleBreak}>10</span> m</p>
+                <p><span onClick={handleBreak}>20</span> m</p>
+                <p><span onClick={handleBreak}>30</span> m</p>
+                <p><span onClick={handleBreak}>40</span> m</p>
+                <p><span onClick={handleBreak}>50</span> m</p>
             </div>
             <div style={{marginTop: '40px'}}>
                 <h3>Sport Details</h3>
@@ -30,7 +33,7 @@ const Calculate = ({sportsData}) => {
                 </div>
                 <div style={{marginTop: '20px'}} className='break-time'>
                     <h4>Break Time</h4>
-                    <p>200 minutes</p>
+                    <p>{breakTime} minutes</p>
                 </div>
                 <button className='activity-btn'>Activity Completed</button>
             </div>
